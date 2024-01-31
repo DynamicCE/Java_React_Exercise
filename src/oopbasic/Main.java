a9d46d82a8f39f7944ed056109934e261132d7ca
Adınız <erkanmurat0632@gmail.com>
2024-01-31 12:46:06 +0300

    oop practise

---
+++ src/oopbasic/Main.java
@@ -0,0 +1,8 @@
+package oopbasic;
+
+public
+class Main {
+    public static void main (String[] args) {
+        System.out.println (Numbers.multiply ( 3,8 ) );
+    }
+}
---
+++ src/oopbasic/Numbers.java
@@ -0,0 +1,23 @@
+package oopbasic;
+
+public
+class Numbers {
+    public static
+    int plus ( int number1, int number2 ) {
+        return number1 + number2;
+    }
+
+    public static
+    int minus ( int number1, int number2 ) {
+        return number1 - number2;
+    }
+
+    public static
+    int multiply ( int number1, int number2 ) {
+        return number1 * number2;
+    }
+
+    public static int divide(int number1,int number2){
+        return number1/number2;
+    }
+}

41c9d62c5aa40dccc960adc486c0ac8fd1925aa1
Adınız <erkanmurat0632@gmail.com>
2024-02-07 19:55:38 +0300

    lesson 3's project

---
+++ src/inheritance2/SmsLogger.java
@@ -0,0 +1,5 @@
+package inheritance2;
+// bir sınıf sadece bir sınıfı extend edebilir ama birden çok interface'i implement edebilir
+public
+class SmsLogger extends Logger {
+}
--- src/kodlamaioProject/User.java
+++ src/kodlamaioProject/User.java
@@ -2,6 +2,9 @@ package kodlamaioProject;
 
 public
 class User {
+
+    //Modelleme yaparken ileriye dönük düşün,yarın bir gün Kurumsal müşteri de eklenirse onun firstName'i lastName'i olmayacak mesela.
+    //firstName,lastName gibi fieldları Instructor ve Student sınıflarında tanımlamam gerekir.
     String userName;
     int userId;
 
